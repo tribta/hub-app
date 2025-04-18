19b1dbca5f2406c7687bd78057f2cb293fe3aac7
Gabriel7721 <tribta@vtc.edu.vn>
2025-04-18 11:28:10 +0700

    11th

--- react-app/src/hooks/useGames.ts
+++ react-app/src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import apiClient from "@/services/api-client";
+import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 
 // creating custom hook: một hook do người dùng tự định nghĩa
@@ -18,10 +19,16 @@ const useGames = () => {
   const [error, setError] = useState("");
 
   useEffect(() => {
+    const controller = new AbortController();
     apiClient
-      .get<FetchGamesResponse>("/games")
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
   }, []);
 
   return { games, error };
