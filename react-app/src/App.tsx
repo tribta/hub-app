177bf8c35326a6bdd80f6f625b82931faa4af994
Gabriel7721 <tribta@vtc.edu.vn>
2025-04-18 10:19:37 +0700

    3rd

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,25 @@
-import { Button, HStack } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <HStack>
-      <Button>Click Me</Button>
-      <Button>Click Me</Button>
-    </HStack>
+    <Grid
+      templateAreas={{
+        base: `"nav nav" "main main"`, // base:320 & lg:1024
+        lg: `"nav nav" "aside main"`,
+      }}>
+      <GridItem gridArea={"nav"} bg={"coral"}>
+        Navigation Bar
+      </GridItem>
+      <GridItem
+        gridArea={"aside"}
+        bg={"gold"}
+        display={{ base: "none", lg: "block" }}>
+        Aside Bar
+      </GridItem>
+      <GridItem gridArea={"main"} bg={"dodgerblue"}>
+        Main Display
+      </GridItem>
+    </Grid>
   );
 };
 
