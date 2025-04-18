172afd6e467282a9de227260245a3842bae29fad
Gabriel7721 <tribta@vtc.edu.vn>
2025-04-18 11:24:25 +0700

    10th

---
+++ react-app/src/hooks/useGames.ts
@@ -0,0 +1,30 @@
+import apiClient from "@/services/api-client";
+import { useEffect, useState } from "react";
+
+// creating custom hook: một hook do người dùng tự định nghĩa
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get<FetchGamesResponse>("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
